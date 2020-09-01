d9e7380432c3fe2ad513ebc2fdf5b1da03aaf17f
avbuka <avbuka@list.ru>
2020-09-01 16:13:51 +0700

    Добавьте файлы проекта.

---
+++ EngineSimulation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EngineSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
